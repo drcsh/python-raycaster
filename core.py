264e531394a5c04399c0f0fbbca604b26444eb1b
drcsh <chris.s.hodder@gmail.com>
2020-05-10 19:07:48 +0100

    Adding game loop and getting it to display on screen.

--- core.py
+++ core.py
@@ -28,9 +28,12 @@ def main():
           "0              0" \
           "0002222222200000"
 
-    surface = pygame.Surface((win_w, win_h))
-    surface.fill((255, 255, 255))
-    game_map = Map(surface, map)
+    pygame.init()
+    screen = pygame.display.set_mode([win_w, win_h])
+
+    background = pygame.Surface((win_w, win_h))
+    background.fill((255, 255, 255))
+    game_map = Map(background, map)
     raycaster = RayCaster(win_w, win_h, fov)
 
     player_x = 3.456
@@ -41,7 +44,29 @@ def main():
     # Raycast a single ray!
     raycaster.cast(game_map, player_x, player_y, player_a)
 
+    screen.blit(game_map.surface, (0, 0))
+
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        # get input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
+                break
+
+        # cap the framerate at 40fps. Also called 40HZ or 40 times per second.
+        clock.tick(40)
+
+        screen.blit(game_map.surface, (0, 0))
+        pygame.display.flip()
+
     pygame.image.save(game_map.surface, "out.bmp")
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
