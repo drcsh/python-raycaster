b6a5752bba6898a3a743cfd23e7e78ad3d8ace18
Chris <chris.s.hodder@gmail.com>
2020-08-01 17:16:02 +0100

    Found a bug with enemies disappearing when the player angle went negative or went over tau. There's also a but with them disappearing when the angle goes > pi which I need to figure out.

--- engine/player.py
+++ engine/player.py
@@ -2,7 +2,7 @@ import math
 
 class Player:
     TURNSPEED = 10 * math.pi / 360
-    MOVESPEED = 0.5
+    MOVESPEED = 0.2
 
     def __init__(self, x, y, angle):
         self.x = x
@@ -21,6 +21,19 @@ class Player:
 
     def turn_left(self):
         self.angle -= self.TURNSPEED
+        self._check_angle()
 
     def turn_right(self):
         self.angle += self.TURNSPEED
+        self._check_angle()
+
+    def _check_angle(self):
+        """
+        when we turn we're modifying the player angle by a fraction of pi. If this angle goes > 2pi or < 0 we need to
+        reset it to prevent other calculations getting thrown out.
+        :return:
+        """
+        if self.angle > math.tau:
+            self.angle -= math.tau
+        elif self.angle < 0:
+            self.angle += math.tau
