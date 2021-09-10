adae10d3219cdd9f96a7a0488fdeb19baf006703
Chris <chris.s.hodder@gmail.com>
2021-09-10 16:28:38 +0100

    WIP: moved player actions out of GameState and into the Input Handler, but I'm not sure that's the right place. I'm thinking about moving them to a separate PlayerBehaviour class that sort of functions like the enemy and bullet behaviour classes. The complication is the relationship between the inputs in InputHandler and the behaviours. It may be best to kill InputHandler and instead create PlayerBehaviours which are invoked based on the gamestate. I.e. if the player is in the level, the keyboard inputs mean something different to if the menu is open.

--- engine/gamestate.py
+++ engine/gamestate.py
@@ -1,9 +1,7 @@
 from engine.behaviours.bullet_behaviour import BulletBehaviour
 from engine.behaviours.enemy_behaviour import EnemyBehaviour
-from engine.game_objects.bullet import Bullet
 from engine.level_objects.level import Level
 from engine.player_objects.player import Player
-from textures.texturemap import TextureMap
 
 
 class GameState:
@@ -34,49 +32,4 @@ class GameState:
         for bullet in self.level.bullets:
             BulletBehaviour.act(bullet, self.level, self.player)
 
-    def player_attack(self):
-        """
-        This action has to go here rather than on the player object, because it creates bullet
-        objects which need to be kept track of by the gamestate.
-        :return:
-        """
 
-        # TODO: Get equipped weapon
-        # TODO: check weapon equipped has ammo
-        # TODO: trigger weapon firing animation
-        # TODO: Get bullet characteristics for weapon
-        b_speed = 0.2
-        b_texturemap = TextureMap.load_common('simple_bullet.png')
-        b_damage = 25
-
-        # create bullet object with self.angle and weapon speed
-        bullet = Bullet(
-            sprite_group=self.level.bullets,
-            x=self.player.x,
-            y=self.player.y,
-            angle=self.player.angle,
-            speed=b_speed,
-            texturemap=b_texturemap,
-            damage=b_damage
-        )
-
-        # trigger bullet move immediately to get it infront of the player and check for impact
-        BulletBehaviour.act(bullet, self.level, self.player)
-
-    def player_moves_forward(self):
-        new_x, new_y = self.player.get_next_forward_position()
-
-        if self.level.location_is_valid(new_x, new_y):
-            self.player.move(new_x, new_y)
-
-    def player_moves_backwards(self):
-        new_x, new_y = self.player.get_next_backward_position()
-
-        if self.level.location_is_valid(new_x, new_y):
-            self.player.move(new_x, new_y)
-
-    def player_turns_left(self):
-        self.player.turn_left()
-
-    def player_turns_right(self):
-        self.player.turn_right()
--- engine/player_objects/input_handler.py
+++ engine/player_objects/input_handler.py
@@ -1,8 +1,11 @@
 import pygame
 import pygame_gui
 
+from engine.behaviours.bullet_behaviour import BulletBehaviour
+from engine.game_objects.bullet import Bullet
 from engine.gamestate import GameState
 from engine.utils.exceptions import GameExitException
+from textures.texturemap import TextureMap
 
 
 class InputHandler:
@@ -25,6 +28,8 @@ class InputHandler:
 
     def handle(self):
 
+        # TODO: different behaviours based on menu active or game active etc.
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 raise GameExitException("Pygame Quit event")
@@ -32,19 +37,60 @@ class InputHandler:
                 raise GameExitException("Player pressed Quit key")
 
             if event.type == pygame.KEYDOWN and event.key == self.SHOOT_K:
-                self.gamestate.player_attack()
+                self.player_attack()
                 continue
             if event.type == pygame.KEYDOWN and event.key == self.FORWARD_K:
-                self.gamestate.player_moves_forward()
+                self.player_moves_forward()
                 continue
             if event.type == pygame.KEYDOWN and event.key == self.BACK_K:
-                self.gamestate.player_moves_backwards()
+                self.player_moves_backwards()
                 continue
             if event.type == pygame.KEYDOWN and event.key == self.LEFT_K:
-                self.gamestate.player_turns_left()
+                self.gamestate.player.turn_left()
                 continue
             if event.type == pygame.KEYDOWN and event.key == self.RIGHT_K:
-                self.gamestate.player_turns_right()
+                self.gamestate.player.turn_right()
                 continue
 
             self.gui_manager.process_events(event)
+
+    def player_attack(self):
+        """
+        This action has to go here rather than on the player object, because it creates bullet
+        objects which need to be kept track of by the gamestate.
+        :return:
+        """
+
+        # TODO: Get equipped weapon
+        # TODO: check weapon equipped has ammo
+        # TODO: trigger weapon firing animation
+        # TODO: Get bullet characteristics for weapon
+        b_speed = 0.2
+        b_texturemap = TextureMap.load_common('simple_bullet.png')
+        b_damage = 25
+
+        # create bullet object with self.angle and weapon speed
+        bullet = Bullet(
+            sprite_group=self.level.bullets,
+            x=self.player.x,
+            y=self.player.y,
+            angle=self.player.angle,
+            speed=b_speed,
+            texturemap=b_texturemap,
+            damage=b_damage
+        )
+
+        # trigger bullet move immediately to get it infront of the player and check for impact
+        BulletBehaviour.act(bullet, self.gamestate.level, self.gamestate.player)
+
+    def player_moves_forward(self):
+        new_x, new_y = self.gamestate.player.get_next_forward_position()
+
+        if self.gamestate.level.location_is_valid(new_x, new_y):
+            self.gamestate.player.move(new_x, new_y)
+
+    def player_moves_backwards(self):
+        new_x, new_y = self.gamestate.player.get_next_backward_position()
+
+        if self.gamestate.level.location_is_valid(new_x, new_y):
+            self.gamestate.player.move(new_x, new_y)
